4c2e7934fbe39ee5538ae191518a995654b7165b
joker <wjb654120914@gmail.com>
2025-08-18 17:33:51 +0800

    webdav

---
+++ src/main/java/cn/joker/webdav/webdav/adapter/WebDavAdapter.java
@@ -0,0 +1,124 @@
+package cn.joker.webdav.webdav.adapter;
+
+import cn.joker.webdav.business.entity.FileBucket;
+import cn.joker.webdav.fileTask.UploadHook;
+import cn.joker.webdav.webdav.adapter.contract.AdapterComponent;
+import cn.joker.webdav.webdav.adapter.contract.IFileAdapter;
+import cn.joker.webdav.webdav.adapter.contract.ParamAnnotation;
+import cn.joker.webdav.webdav.entity.FileResource;
+import com.alibaba.fastjson2.JSONObject;
+import com.github.sardine.DavResource;
+import com.github.sardine.Sardine;
+import com.github.sardine.SardineFactory;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.List;
+
+@AdapterComponent(title = "WebDav")
+public class WebDavAdapter implements IFileAdapter {
+
+
+    @Getter
+    @Setter
+    @ParamAnnotation(label = "服务器地址")
+    private String url;
+
+    @Getter
+    @Setter
+    @ParamAnnotation(label = "服务器端口号")
+    private String prot;
+
+
+    @Getter
+    @Setter
+    @ParamAnnotation(label = "用户名")
+    private String username;
+
+    @Getter
+    @Setter
+    @ParamAnnotation(label = "密码")
+    private String password;
+
+
+    private Sardine getSardine(FileBucket fileBucket) throws IOException {
+        JSONObject jsonObject = fileBucket.getFieldJson();
+        String url = jsonObject.getString("url")  + ":" + jsonObject.getString("prot") + fileBucket.getSourcePath();
+        jsonObject.put("davUrl", url);
+        Sardine sardine = SardineFactory.begin(jsonObject.getString("username"), jsonObject.getString("password"));
+        return sardine;
+    }
+
+
+    @Override
+    public boolean hasPath(FileBucket fileBucket, String path) {
+        List<FileResource> list = null;
+        try {
+            list = propFind(fileBucket, path, false);
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e.getMessage());
+        }
+
+        if (list == null || list.isEmpty()) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    @Override
+    public FileResource getFolderItself(FileBucket fileBucket, String uri) throws IOException {
+        return null;
+    }
+
+    @Override
+    public List<FileResource> propFind(FileBucket fileBucket, String uri, boolean refresh) throws IOException {
+        Sardine sardine = getSardine(fileBucket);
+        List<DavResource> resources = sardine.list(fileBucket.getFieldJson().getString("davUrl") + uri);
+
+        return List.of();
+    }
+
+    @Override
+    public void get(FileBucket fileBucket, String path) throws Exception {
+
+    }
+
+    @Override
+    public void put(FileBucket fileBucket, String path, Path tempFilePath, UploadHook hook) throws Exception {
+
+    }
+
+    @Override
+    public void delete(FileBucket fileBucket, String path) throws IOException {
+
+    }
+
+    @Override
+    public void mkcol(FileBucket fileBucket, String path) throws IOException {
+
+    }
+
+    @Override
+    public void move(FileBucket fromFileBucket, String fromPath, FileBucket toFileBucket, String toPath) throws IOException {
+
+    }
+
+    @Override
+    public void copy(FileBucket fromFileBucket, String fromPath, FileBucket toFileBucket, String toPath) throws IOException {
+
+    }
+
+    @Override
+    public String getDownloadUrl(FileBucket fileBucket, String path) throws IOException {
+        return "";
+    }
+
+    @Override
+    public String workStatus(FileBucket fileBucket) {
+        return "";
+    }
+}
