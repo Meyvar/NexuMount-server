a03b4f7c630d6d48ebe8bb8367e9ee2a97952ff6
joker <wjb654120914@gmail.com>
2025-08-22 20:27:10 +0800

    infuse兼容问题处理

--- src/main/java/cn/joker/webdav/webdav/filter/WebDavFilter.java
+++ src/main/java/cn/joker/webdav/webdav/filter/WebDavFilter.java
@@ -1,10 +1,12 @@
 package cn.joker.webdav.webdav.filter;
 
 
+import cn.dev33.satoken.stp.StpUtil;
 import cn.joker.webdav.business.service.ISysUserService;
 import cn.joker.webdav.webdav.service.IWebDavService;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -39,7 +41,7 @@ public class WebDavFilter extends OncePerRequestFilter {
                 StringUtils.hasText(depth)
                         || StringUtils.hasText(basic)
                         || (StringUtils.hasText(ua) && ua.contains("WebDAV"))
-                        || request.getHeader("cookie").contains("Authorization-Key")
+                        || request.getHeader("cookie").contains("webdav-key")
         ) {
 
             if (!StringUtils.hasText(basic)) {
@@ -47,12 +49,30 @@ public class WebDavFilter extends OncePerRequestFilter {
                 response.setHeader("WWW-Authenticate", "Basic realm=\"\"");
                 return;
             } else {
+
+                if (request.getHeader("cookie") != null && request.getHeader("cookie").contains("webdav-key")) {
+
+                    Cookie[] cookies = request.getCookies();
+                    if (cookies != null) {
+                        for (Cookie cookie : cookies) {
+                            if (cookie.getName().equals("webdav-key")) {
+                                basic = "Basic " + cookie.getValue();
+                                break;
+                            }
+                        }
+                    }
+                }
+
+
+                String authorization = basic;
                 basic = basic.replace("Basic ", "");
                 basic = new String(Base64.getDecoder().decode(basic));
                 String[] user = basic.split(":");
                 try {
                     sysUserService.login(user[0], user[1]);
+                    response.addCookie(new Cookie("webdav-key", authorization.replace("Basic ", "")));
                 } catch (Exception e) {
+                    e.printStackTrace();
                     response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
                     response.setHeader("WWW-Authenticate", "Basic realm=\"\"");
                     return;
